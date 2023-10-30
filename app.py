3a2c344219f5d6c89b4b0812ff62e04bd2ba5cbb
fredrick-simon <140687049+fredrick-simon@users.noreply.github.com>
2023-10-30 23:21:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,57 +1,34 @@
-import streamlit as st
+from flask import Flask, render_template, request
 import tensorflow as tf
 import numpy as np
 
-# Configure the Streamlit app
-st.set_page_config(
-    page_title="Sales Prediction",
-    page_icon="📈",
-    layout="centered",
-    initial_sidebar_state="expanded",
-)
-
-# Set the CSS styles for the app
-st.markdown(
-    """
-    <style>
-    body {
-        background-color: black;
-        color: orange;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True,
-)
-
-st.title("Sales Prediction")
+app = Flask(__name__)
 
 # Load the saved ANN model
 model = tf.keras.models.load_model("1.h5")
 
-# Define the input fields
-country = st.number_input("Enter the country code:", value=0)
-store = st.number_input("Enter the store code:", value=0)
-product = st.number_input("Enter the product code:", value=0)
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-# Define a function to preprocess the input values and make a prediction
-def predict_sales(country, store, product):
-    # Preprocess the input values
-    inputs = np.array([country, store, product]).reshape(1, -1)
+@app.route('/predict', methods=['POST'])
+def predict():
+    country = int(request.form['country'])
+    store = int(request.form['store'])
+    product = int(request.form['product'])
 
-    # Make a prediction using the loaded model
-    prediction = model.predict(inputs)
+    # Define a function to preprocess the input values and make a prediction
+    def predict_sales(country, store, product):
+        # Preprocess the input values
+        inputs = np.array([country, store, product]).reshape(1, -1)
 
-    return prediction[0][0]
+        # Make a prediction using the loaded model
+        prediction = model.predict(inputs)
 
-# Call the predict_sales function when the user clicks the "Predict" button
-if st.button("Predict"):
+        return prediction[0][0]
+
     prediction = predict_sales(country, store, product)
-    st.write("### Prediction Result")
-    st.write(f"The predicted sales for this product is {prediction:.2f}.")
+    return f"The predicted sales for this product is {prediction:.2f}."
 
-# Display a sample prediction if the user clicks the "Use Sample Data" button
-if st.button("Use Sample Data"):
-    country, store, product = 0, 0, 0  # Replace with your own sample inputs
-    prediction = predict_sales(country, store, product)
-    st.write("### Prediction Result")
-    st.write(f"The predicted sales for this product is {prediction:.2f}.")
+if __name__ == '__main__':
+    app.run(debug=True)
